bd41df064a426e757113e730d7fe86983d1f731a
Dwayne Ballard <dwayne.ballard123@gmail.com>
2021-12-27 21:15:01 +0000

    lodown progress

--- index.js
+++ index.js
@@ -79,10 +79,11 @@ module.exports.typeOf = typeOf;
  * first: The first function take in an array and a number and returns the first 
  * item/items in the array with the amount returned equal to the number parameter. 
  * 
- * @param { array } array: 
- * @param { number } number: 
+ * @param { array } array: Function takes in an array of values
+ * @param { number } number: Function takes in a number that equals the amount of
+ * items we want to return from the array.
  * 
- * @returns { a value or array }: The function returns the first value of array if 
+ * @returns { a value or array }: The function returns the FIRST value of array if 
  * no parameter is given or if it is negative; OR it will return the ORIGINAL ARRAY
  * if the number parameter is greater than the length of our array parameter, OR if 
  * the number parameter is greater than 1, it will return a NEW ARRAY with a length
@@ -111,3 +112,66 @@ function first(array, number) {
 }
 
 module.exports.first = first;
+
+/**
+ * last: The last function takes in an array and a number and returns the last item/items
+ * in the array with the amount of items returned equal to the number parameter starting
+ * BACKWARDS in the original array.
+ * 
+ * @param { array } array: Function takes in an array of values.
+ * @param { number } number: Function takes in a number that equals the length/amount of items
+ * we want return from our input array.
+ * 
+ * @returns { a value or array }:The function returns the LAST value of array if 
+ * no parameter is given or if it is negative; OR it will return the ORIGINAL ARRAY
+ * if the number parameter is greater than the length of our array parameter, OR if 
+ * the number parameter is greater than 1, it will return a NEW ARRAY with a length
+ * equal to that number and including values starting at the END of the original array
+ * and discard the remaining items in front.  
+ * 
+ */
+
+function last(array, number) {
+    // if input array is not an array or if input parameter is negative, return empty array
+    if (!Array.isArray(array) || number < 0) {
+        return [];
+        // if number parameter is not given or is not a number, return LAST element in input array
+    } else if (number === undefined || typeof number !== "number") {
+        return array[array.length - 1];
+        // if number parameter is greater than array length, return entire array input
+    } else if (number > array.length) {
+        return array;
+        // now return a new array that starts at LAST INDEX and ends at the number input
+        // slicing at a negative index will return an array that starts at that index(deleting that index) counting backward,
+        // and printing to array from there onwards to the end of the array. 
+    } else {
+        return array.slice(-number);
+    }
+}
+
+module.exports.last = last;
+
+/**
+ * indexOf: The indexOf function takes in an array and a value and returns the 
+ * index that corresponds with the FIRST OCCURENCE of that input value in the array.
+ * 
+ * @param { array } array: Function takes in an array of values to iterate through.
+ * @param { any value } value: Function takes in a value that we will be searching for
+ * in the array.
+ * 
+ * @returns { number }: The function returns a number that corresponds with the INDEX
+ * of the value in the array. If the array does NOT CONTAIN the value we are looking for,
+ * the number -1 is returned.
+ * 
+ */
+
+function indexOf(array, value) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+module.exports.indexOf = indexOf;
